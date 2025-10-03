6bf802ef302ab8186ef262fe874004ac402a457e
sk98789 <s_korzekwa@uncg.edu>
2025-10-02 20:48:54 -0400

    added statistics functionality

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ var playerChoice;
 var image_choices = ["rock.png", "paper.png", "scissors.png"];
 var animation;
 var results = document.querySelector("#res_display");
-var button = document.querySelector("button");
+var button = document.querySelector("#play_button");
 var  winCount = document.querySelector("#stats_wins");
 var  tieCount = document.querySelector("#stats_ties");
 var  lossCount = document.querySelector("#stats_losses");
+var statsButton = document.querySelector("#stats_button");
 
 
 button.addEventListener('click', () => {
@@ -18,13 +19,14 @@ button.addEventListener('click', () => {
     player_choices[playerChoice].classList.replace("player_selected", "not_selected");
     results.textContent = "";
     choice_selected = false;
-
 })
+
+statsButton.addEventListener('click', refreshStats);
+
 
 player_choices.forEach( choice => {
     choice.addEventListener('click', selectChoice);
 });
-console.log(winCount);
 function selectChoice(event){
     if(!choice_selected){
         choice_selected = true;
@@ -62,32 +64,39 @@ function calculateWinner(cpuAnswer){
 
     if(playerChoice === cpuAnswer){
         results.textContent = "Tie!";
-        tieCount.textContent = (Num(tieCount.textContent)+1) + "";
+        tieCount.textContent = (parseInt(tieCount.textContent)+1);
     }
     else if(playerChoice> cpuAnswer){
         if(playerChoice === 2 && cpuAnswer === 0){
             results.textContent = "You Lose!";
-             lossCount.textContent = (Num(lossCount.textContent)+1).toString;
+             lossCount.textContent = (parseInt(lossCount.textContent)+1);
             
 
         }
         else{
             results.textContent = "You Win!";
-            winCount.textContent = (Num(winCount.textContent)+1).toString;
+            winCount.textContent = (parseInt(winCount.textContent)+1);
         }
     }
     else{
          if(playerChoice === 0 && cpuAnswer === 2){
             results.textContent = "You Win!";
-            winCount.textContent = (Num(winCount.textContent)+1).toString;
+            winCount.textContent = (parseInt(winCount.textContent)+1);
         }
         else{
             results.textContent = "You Lose!";
-            lossCount.textContent = (Num(lossCount.textContent)+1).toString;
+            lossCount.textContent = (parseInt(lossCount.textContent)+1);
         }
 
     }
 
 
+
+}
+
+function refreshStats(){
+    winCount.textContent = 0;
+    tieCount.textContent = 0;
+    lossCount.textContent = 0;
 
 }
